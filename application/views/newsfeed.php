197cf6b98f7453d371766d7f18312e2ebb5572d2
iansisante <iansisante@gmail.com>
2022-05-20 17:54:05 +0800

    phpfication

---
+++ application/views/newsfeed.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsfeed</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="default_controller">Home</a>
+        </li>
+        <li>notifications</li>
+        <li>
+            <a href="profile">Profile</a>
+        </li>
+        <li>
+            <a href="signin">Sign in</a>
+        </li>
+        <li>
+            <a href="signup">Sign up</a>
+        </li>
+    </ul>
+</body>
+</html>
