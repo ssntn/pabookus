ae23a9251d843e83a196f0f2a1e862fd032d126f
iansisante <iansisante@gmail.com>
2022-05-22 17:53:50 +0800

    profile details
    unfinished

--- application/views/profile.php
+++ application/views/profile.php
@@ -12,7 +12,7 @@
         <li>
             <a href="newsfeed">Home</a>
         </li>
-        <li>notifications</li>
+        <li><button>notifications</button></li>
         <li>
             <a href="profile">Profile</a>
         </li>
@@ -24,16 +24,43 @@
         </li>
     </ul>
 
-    <p>Company Name</p>
-    <p>Email</p>
-    <p>Contact</p>
-    <p>Address</p>
-    <p>Industry</p>
-    <p>Owner</p>
-    <p>Birthday</p>
+    <!-- THIS IS PROFILE PICTURE -->
+    <div id="" style="width:200px;height:200px;background-color:red"></div>
+
+    <p class="inf_title">Name:</p>
+    <p  class="u_inf" id="name_lbl">Company Name</p>
+    <input type="text" name="name_edt" id="name_edt">
+
+    <button>Edit info</button>
+    
+    <p class="inf_title">Email:</p>
+    <p class="u_inf" id="email">Email</p>
+    <input type="text" name="email_edt" id="email_edt">
+    
+    <p class="inf_title">Contact numbers:</p>
+    <p class="u_inf" id="contact">Contact</p>
+    <input type="text" name="contact_edt" id="contact_edt">
+    
+    <p class="inf_title">Address</p>
+    <p class="u_inf" id="address">Address</p>
+    <input type="text" name="address_edt" id="address_edt">
+    
+    <p class="inf_title">Industry</p>
+    <p class="u_inf" id="industry">Industry</p>
+    <input type="text" name="industry_edt" id="industry_edt">
+    <select name="industry_list" id="industry_edts"></select>
+
+    <p class="inf_title">Owner</p>
+
+    <p>Birthday</p> <button>edit</button>
+    <input type="date" name="birthdate" id="birthdate">
     <p>Bio</p>
-    <p>Links</p>
-    <p>Schedule</p>
+    <ul>links
+        <li>
+            <a href="newsfeed">Home</a>
+        </li>
+    </ul>
+    <p><button><a href="schedule">Schedule</a></button></p>
     <p>Reviews</p>
     
 </body>
