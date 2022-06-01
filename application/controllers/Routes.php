9e89a8cf06400d8f7cec2bb5bec91e2fa41e1f8e
iansisante <iansisante@gmail.com>
2022-06-01 13:18:19 +0800

    session test

--- application/controllers/Routes.php
+++ application/controllers/Routes.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Routes extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+    }
     
 	public function index()
 	{
--- application/views/newsfeed.php
+++ application/views/newsfeed.php
@@ -1,3 +1,14 @@
+<?php 
+if($this->session->userdata('UserLoginSession'))
+{
+    $udata = $this->session->userdata('UserLoginSession');
+}
+else
+{
+    redirect(base_url('signin'));
+}
+?>
 
 <body>
 <p>Newsfeed</p>
+<p><?php echo $udata['email'] ?></p>
