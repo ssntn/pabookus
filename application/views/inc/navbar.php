a26dae881ee5f419307fa8263d92c64913af7cf8
iansisante <iansisante@gmail.com>
2022-06-03 16:34:20 +0800

    header/navbar session based output

--- application/views/inc/navbar.php
+++ application/views/inc/navbar.php
@@ -8,19 +8,36 @@
         <a href="newsfeed">Home</a>
     </li>
     <li>
-        <button>notifications</button>
+        <a href=
+            <?php 
+                if(isset($udata)){
+                    
+        
+
+                    // company 
+                    $ut = ($udata['user_type']=='client')?'1':'2'; 
+                    echo "profile?id=".$udata['id']."&ut=".$ut;
+                }else
+                    echo 'signin';                
+            ?>
+        >Profile</a>
     </li>
-    <li>
-        <a href="profile?id=<?php echo $udata['id']?>&ut=2">Profile</a>
-    </li>
-    <li>
-        <a href="signin">Sign in</a>
-    </li>
-    <li>
-        <a href="signup">Sign up</a>
-    </li>
-    <li>
-        <!-- user type: 1 = company, 2 = clients -->
-        <a href="logout">Logout</a>
-    </li>
+
+    <?php if(!isset($udata)){ ?>
+        <li>
+            <a href="signin">Sign in</a>
+        </li>
+        <li>
+            <a href="signup">Sign up</a>
+        </li>
+        
+    <?php } else { ?>
+        <li>
+            <button>notifications</button>
+        </li>
+        <li>
+            <!-- user type: 1 = client, 2 = company -->
+            <a href="logout">Logout</a>
+        </li>
+    <?php } ?>
 </ul>
--- application/views/newsfeed.php
+++ application/views/newsfeed.php
@@ -1,9 +1,8 @@
 <?php 
     if($this->session->userdata('UserLoginSession'))
         $udata = $this->session->userdata('UserLoginSession');
-    else redirect(base_url('signin'));
 ?>
 
 <body>
 <p>Newsfeed</p>
-<p><?php if(isset($udata))echo $udata['email'] ?></p>
+<p><?php if(isset($udata)) echo $udata['email'] ?></p>
