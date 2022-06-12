97a9f9ea2e012611a9bcb0a5bbb3922438522d78
matt <matthewperrybustarde@gmail.com>
2022-06-12 17:42:09 +0800

    added search bar design

--- application/views/newsfeed.php
+++ application/views/newsfeed.php
@@ -3,6 +3,18 @@
         $udata = $this->session->userdata('UserLoginSession');
 ?>
 
-<body>
-<p>Newsfeed</p>
+<div class="container" style="justify-content: center; background-color: #A3CEF1; margin-top: 20px; border-radius: 10px;">
+    
+    <div class="search">
+        <form class="search-form">
+            <input type="text" placeholder="Search...">
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    <div>
+
+    </div>
+</div>
+
 <p><?php if(isset($udata)) echo $udata['email'] ?></p>
