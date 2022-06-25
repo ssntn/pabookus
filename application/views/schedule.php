8671217e9f84675e3b6d8b36850ba0866591be60
iansisante <iansisante@gmail.com>
2022-06-25 13:36:54 +0800

    added ci calendar

--- application/views/schedule.php
+++ application/views/schedule.php
@@ -8,3 +8,14 @@
 <p><?php echo $key_service['name'];?></p>
 <p><?php echo $key_service['price'];?></p>
 <p><?php echo $key_company['company_name']; ?></p>
+<div id="calendar_div">
+
+</div>
+
+<script>
+    
+    $.post("<?=base_url('Calendar/get_calendar')?>",{}, function(data){
+        
+        $("#calendar_div").append("<div>"+data+"</div");
+    });
+</script>
