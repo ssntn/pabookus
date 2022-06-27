2aaf551d935e4ce2b047e3024288eafa993769ab
iansisante <iansisante@gmail.com>
2022-06-28 00:47:54 +0800

    cleaned code

--- application/views/newsfeed.php
+++ application/views/newsfeed.php
@@ -12,12 +12,12 @@ if(isset($key_services)){
     <div style="background-color:#99f;width:50%">
         <img src="#" alt="Service Picture"><br><br><br>
 
-        <p><a href="schedule/<?php echo $s["company_id"]."/".$s["id"];?>">
-            <?php echo $s["name"];?></a>
+        <p><a href="schedule/<?php echo $s["company_id"]."/".$s["id"];?>"><?php 
+            echo $s["name"];?></a>
         </p>
-        <p><a href="<?php echo base_url()."profile/company/".$s["company_id"];?>">
-
-            <?php echo $s["company_name"];?></a>
+        
+        <p><a href="<?php echo base_url()."profile/company/".$s["company_id"];?>"><?php 
+            echo $s["company_name"];?></a>
         </p>
 
     </div>
